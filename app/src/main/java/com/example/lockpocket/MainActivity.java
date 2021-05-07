42c51996ae83a1310af01c3c67b60e41ad7cd376
EJ <dw0815@naver.com>
2021-05-07 22:12:45 +0900

    aa

--- app/src/main/java/com/example/lockpocket/MainActivity.java
+++ app/src/main/java/com/example/lockpocket/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-dddd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
