18438414756bc6738ac7fe432a1b5d4affeee56b
kimhyohee <hyohi8913@naver.com>
2021-05-11 00:56:26 +0900

    0510

--- app/src/main/java/com/example/lockpocket/menu.java
+++ app/src/main/java/com/example/lockpocket/menu.java
@@ -1,19 +1,16 @@
 package com.example.lockpocket;
 
-
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-
-public class MainActivity extends AppCompatActivity {
+public class menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.header);
 
 
     }
 }
-
