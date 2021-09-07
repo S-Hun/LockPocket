e3fe7c75167ddf244feb0d38067be2d0acfcc19b
S-Hun <dinky_apple@naver.com>
2021-09-07 18:47:19 +0900

    Update class design

--- app/src/main/java/com/example/lockpocket/utils/LockTable.java
+++ app/src/main/java/com/example/lockpocket/utils/LockTable.java
@@ -4,19 +4,14 @@ import android.util.Log;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import com.example.lockpocket.utils.AttrList;
 
 public class LockTable {
 
+    int _idStack = 1;
     // Move to attributes in Type class.
-    static Type[] typeList = {
-            new Type(1, 1, 0),
-            new Type(1, 1, 0),
-            new Type(1, 1, 0),
-            new Type(3, 2, 0),
-            new Type(3, 4, 0)
-    };
 
-    boolean[][] disable = new boolean[5][6];
+    int[][] disable = new int[5][6];
 
     static private class Widget {
         static class Pos {
@@ -53,20 +48,15 @@ public class LockTable {
         int attr;
         Pos pos;
 
-        int _idStack = 1;
-
-        Widget(int sx, int sy, byte _type){
+        Widget(int _id, int sx, int sy, byte _type){
+            pos = new Pos();
             setPoint(sx, sy);
-            id = generateID();
+            id = _id;
             attr = 0x0000;
             type = _type;
         }
 
         private void setPoint(int _x, int _y){ pos.setX(_x); pos.setY(_y); }
-        private int generateID(){
-            // Here is id generate algorithm.
-            return _idStack++;
-        }
         public int getID(){ return id; }
         public int getX() { return pos.getX(); }
         public int getY() { return pos.getY(); }
@@ -75,20 +65,28 @@ public class LockTable {
 
     LinkedList<Widget> widgets = new LinkedList<>();
 
+    LockTable() { for(int i=0; i<30; i++) disable[i/6][i%6] = -1; widgets = new LinkedList<>(); }
+
+    private int generateID(){
+        // Here is id generate algorithm.
+        return _idStack++;
+    }
+
     public int insert(int _x, int _y, byte _type) {
-        int w = typeList[_type].w, h = typeList[_type].h;
+        int w = AttrList.getWidgetID(_type).w, h = AttrList.getWidgetID(_type).h;
+        int _id = generateID();
 
         for(int i=_y; i < _y + h; i++) {
             for (int j = _x; j < _x + w; j++)
-                if (disable[i][j]) return -1; // return widget
+                if (disable[i][j] > 0) return -1; // return widget
         }
 
-        widgets.add(new Widget(_x, _y, _type));
+        widgets.add(new Widget(_id, _x, _y, _type));
 
         // fill blank space.
         for(int i=_y; i < _y + h; i++){
             for(int j=_x; j < _x + w; j++)
-                disable[i][j] = true;
+                disable[i][j] = _id;
         }
         return 0;
     }
@@ -107,14 +105,22 @@ public class LockTable {
             }
         }
 
-        int w = typeList[type].w, h = typeList[type].h;
+        int w = AttrList.getWidgetID(type).w, h = AttrList.getWidgetID(type).h;
         // fill blank space.
         for(int i=y; i < y + h; i++){
             for(int j=x; j < x + w; j++)
-                disable[i][j] = false;
+                disable[i][j] = -1;
         }
 
         return -1;
+    }
+
+    public void print() {
+        Iterator<Widget> iterator = widgets.iterator();
+        while(iterator.hasNext()) {
+            Widget temp = iterator.next();
+            System.out.println(Integer.toString(temp.getID()) + " " + Integer.toString(temp.getType()));
+        }
     }
 
 }
--- app/src/main/java/com/example/lockpocket/utils/Type.java
+++
@@ -1,6 +0,0 @@
-package com.example.lockpocket.utils;
-
-public class Type {
-    public int w, h, attr;
-    Type(int _w, int _h, int _attr) {}
-}
