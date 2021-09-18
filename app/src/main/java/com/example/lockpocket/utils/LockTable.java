103a214f1edb828553e88e1773dab9cbe25dd1c3
S-Hun <dinky_apple@naver.com>
2021-09-18 11:05:05 +0900

    Update new view of lockscreen

--- app/src/main/java/com/example/lockpocket/utils/LockTable.java
+++ app/src/main/java/com/example/lockpocket/utils/LockTable.java
@@ -4,16 +4,9 @@ import android.util.Log;
 
 import java.util.Iterator;
 import java.util.LinkedList;
-import com.example.lockpocket.utils.AttrList;
 
-public class LockTable {
-
-    int _idStack = 1;
-    // Move to attributes in Type class.
-
-    int[][] disable = new int[5][6];
-
-    static private class Widget {
+public interface LockTable {
+    class Widget {
         static class Pos {
             byte value = 0x00;
             int x = 0, y = 0;
@@ -62,65 +55,14 @@ public class LockTable {
         public int getY() { return pos.getY(); }
         public int getType() { return type; }
     }
-
-    LinkedList<Widget> widgets = new LinkedList<>();
-
-    LockTable() { for(int i=0; i<30; i++) disable[i/6][i%6] = -1; widgets = new LinkedList<>(); }
-
-    private int generateID(){
+    default int generateID() {
         // Here is id generate algorithm.
-        return _idStack++;
-    }
-
-    public int insert(int _x, int _y, byte _type) {
-        int w = AttrList.getWidgetID(_type).w, h = AttrList.getWidgetID(_type).h;
-        int _id = generateID();
-
-        for(int i=_y; i < _y + h; i++) {
-            for (int j = _x; j < _x + w; j++)
-                if (disable[i][j] > 0) return -1; // return widget
-        }
-
-        widgets.add(new Widget(_id, _x, _y, _type));
-
-        // fill blank space.
-        for(int i=_y; i < _y + h; i++){
-            for(int j=_x; j < _x + w; j++)
-                disable[i][j] = _id;
-        }
         return 0;
     }
-
-    public int remove(int _id) {
-        int x = 0, y = 0, type = 0;
-        Iterator<Widget> iterator = widgets.iterator();
-        while(iterator.hasNext()) {
-            Widget temp = iterator.next();
-            if(temp.getID() == _id){
-                x = temp.getX();
-                y = temp.getY();
-                type = temp.getType();
-                widgets.remove(temp);
-                break;
-            }
-        }
-
-        int w = AttrList.getWidgetID(type).w, h = AttrList.getWidgetID(type).h;
-        // fill blank space.
-        for(int i=y; i < y + h; i++){
-            for(int j=x; j < x + w; j++)
-                disable[i][j] = -1;
-        }
-
-        return -1;
+    default int insert(int _x, int _y, byte _type){
+        return 0;
     }
-
-    public void print() {
-        Iterator<Widget> iterator = widgets.iterator();
-        while(iterator.hasNext()) {
-            Widget temp = iterator.next();
-            System.out.println(Integer.toString(temp.getID()) + " " + Integer.toString(temp.getType()));
-        }
+    default int remove(int _id){
+        return 0;
     }
-
 }
--- app/src/main/java/com/example/lockpocket/utils/WidgetList.java
+++ app/src/main/java/com/example/lockpocket/utils/WidgetList.java
@@ -1,10 +1,11 @@
 package com.example.lockpocket.utils;
 
-public class AttrList {
+public class WidgetList {
     static class Type {
-        public int w, h, attr;
+        final public int w, h, attr;
         Type(int _w, int _h, int _attr) {w = _w; h = _h; attr = _attr;}
     }
+    // TYPE MAX 127
     public static final byte WG_WIFI = 0;
     public static final byte WG_DATA = 1;
     public static final byte WG_PLANE = 2;
@@ -15,7 +16,7 @@ public class AttrList {
     public static final Type PLANE = new Type(1, 1, 0);
     public static final Type FLASH = new Type(1, 1, 0);
 
-    public static String getWidgetName(int type){
+    public static String getName(int type){
         if(type == 1) return "WIFI";
         else if(type == 2) return "DATA";
         else if(type == 3) return "PLANE";
@@ -23,7 +24,7 @@ public class AttrList {
         else return null;
     }
 
-    public static Type getWidgetID(int type){
+    public static Type getID(int type){
         if(type == 1) return WIFI;
         else if(type == 2) return DATA;
         else if(type == 3) return PLANE;
