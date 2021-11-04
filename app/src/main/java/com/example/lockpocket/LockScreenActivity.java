cf913ec85d470b202306cb069d4c29aa03ab1ce6
Geonhee12 <chk971212@naver.com>
2021-11-05 01:57:54 +0900

    ClockSetup

--- app/src/main/java/com/example/lockpocket/LockScreenActivity.java
+++ app/src/main/java/com/example/lockpocket/LockScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.lockpocket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,6 +13,8 @@ import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.util.Size;
 import android.view.Gravity;
@@ -27,6 +30,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.lockpocket.lockmethod.UnlockBar;
@@ -36,6 +40,7 @@ import com.example.lockpocket.utils.LockTable;
 import com.example.lockpocket.utils.PreferenceManager;
 import com.example.lockpocket.utils.TableFloater;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class LockScreenActivity extends AppCompatActivity {
@@ -44,7 +49,7 @@ public class LockScreenActivity extends AppCompatActivity {
 
     LockTable lockTableObject;
     ViewGroup lockTableLayout;
-
+    ProgressHandler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -109,6 +114,7 @@ public class LockScreenActivity extends AppCompatActivity {
                 lockTableSetup();
             }
         });
+
     }
 
     void lockTableSetup() {
@@ -122,8 +128,61 @@ public class LockScreenActivity extends AppCompatActivity {
                 lockTableObject.stringToTable(template, new Size(p.x, p.y));
             }
         }
+        Clockget();
     }
 
+    TextView tv1 = null;
+    TextView tv2 = null;
+    String Hour;
+    String Min;
+    public void Clockget(){
+        SimpleDateFormat hour = new SimpleDateFormat("HH");
+        SimpleDateFormat min = new SimpleDateFormat("mm");
+        Log.d("hour", "a" + hour);
+        Log.d("hour", "a" + min);
+        for(int i=0; i<lockTableLayout.getChildCount(); i++)
+        {
+            if(lockTableLayout.getChildAt(i).getTag().equals("click"))
+            {
+                ViewGroup vg = (ViewGroup)lockTableLayout.getChildAt(i);
+                tv1 = (TextView) vg.getChildAt(0);
+                tv1 = (TextView) vg.getChildAt(1);
+
+            }
+        }
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        Hour = hour.format(new Date(System.currentTimeMillis()));
+                        Min = min.format(new Date(System.currentTimeMillis()));
+                        Log.d("ss", Min);
+                        Message message = handler.obtainMessage();
+                        handler.sendMessage(message);
+                        Log.d("hour", Hour);
+                        Log.d("min", Min);
+
+                        Thread.sleep(60000);
+                    } catch (InterruptedException ex) {
+
+                    }
+                }
+            }
+        });
+        thread.start();
+    }
+    class ProgressHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+
+            tv1.setText(Hour);
+            tv2.setText(Min);
+            Log.d("hour", "a" + tv1);
+            Log.d("hour", "a" + tv2);
+        }
+    }
     @Override
     public void onAttachedToWindow() {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|
